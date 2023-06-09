83dc3aac70064b4d5d294e0f3b06b93ac129915a
Micah Njeru <micahnjeru1@gmail.com>
2023-06-09 10:53:44 +0300

    Modified data struct, modified content view

---
+++ Bucketlist/Location.swift
@@ -0,0 +1,36 @@
+//
+//  Location.swift
+//  Bucketlist
+//
+//  Created by Micah Jesse Njeru on 09/06/2023.
+//
+
+import CoreLocation
+import Foundation
+
+
+struct Location: Identifiable, Codable, Equatable {
+    let id: UUID
+    var name: String
+    var description: String
+    let latitude: Double
+    let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    // Sample location for testing in previews
+    static let example = Location(
+        id: UUID(),
+        name: "Buckingham Palace",
+        description: "Where Queen Elizabeth lives with her dorgis",
+        latitude: 51.501,
+        longitude: -0.141
+    )
+    
+    // Custom equitable method
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
