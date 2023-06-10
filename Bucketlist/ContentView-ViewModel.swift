88fb707c7261f78e8070bbfcba2da459c8af58aa
Micah Njeru <micahnjeru1@gmail.com>
2023-06-10 11:02:19 +0300

    Modified model view to add file path

--- Bucketlist/ContentView-ViewModel.swift
+++ Bucketlist/ContentView-ViewModel.swift
@@ -15,7 +15,7 @@ extension ContentView {
             center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
             span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25)
         )
-        @Published private(set) var locations = [Location]()
+        @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
         
         let savePath = FileManager.documentDirectory.appendingPathComponent("SavedPlaces")
@@ -29,7 +29,14 @@ extension ContentView {
             }
         }
         
-        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomicWrite, .completeFileProtection])
+            } catch {
+                print(("Unable to save data."))
+            }
+        }
         
         func addLocation() {
             let newLocation = Location(
@@ -40,6 +47,7 @@ extension ContentView {
                 longitude: mapRegion.center.longitude
             )
             locations.append(newLocation)
+            save()
         }
         
         func update(location: Location) {
@@ -47,6 +55,7 @@ extension ContentView {
             
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
+                save()
             }
         }
     }
